b9dfa50f4149c4eb236a94d00dab64a9ae384da1
xu_yabin <yabin.xu@hp.com>
2015-12-10 10:51:04 +0800

    fix bug12, add comments bbb

--- helloWorld/src/helloWorld/HelloWorldTest.java
+++ helloWorld/src/helloWorld/HelloWorldTest.java
@@ -1,6 +1,6 @@
 package helloWorld;
 
 public class HelloWorldTest {
-	// TODO Auto-generated method stub
-	System.out.println("Hello, World, Test06, Test09");
+	// TODO Auto-generated method stub bbb
+	System.out.println("Hello, World, Test06, Test09, Test10");
 }
